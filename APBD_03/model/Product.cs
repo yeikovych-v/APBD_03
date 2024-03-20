6be019df34a876ffffe1a4b2e95eb2da07bbbf50
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:07:22 +0100

    Added Product enum.

---
+++ APBD_03/model/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace APBD_03.model;
+
+public enum Product
+{
+    Bananas,
+    Chocolate,
+    Fish,
+    Meat,
+    IceCream,
+    FrozenPizza,
+    Cheese,
+    Sausages,
+    Butter, 
+    Eggs
+}
