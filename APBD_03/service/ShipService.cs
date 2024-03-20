1d5ac6c386c9234cb243cb8eba3fe2be90fde9e4
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:06:38 +0100

    Added Service to handle operations with ships.

---
+++ APBD_03/service/ShipService.cs
@@ -0,0 +1,16 @@
+﻿using APBD_03.model;
+using APBD_03.model.containers;
+
+namespace APBD_03.service;
+
+public static class ShipService
+{
+    public static void TransferContainerFromTo(Ship from, Ship to, Container container)
+    {
+        if (from.Contains(container))
+        {
+            from.Remove(container);
+            to.Add(container);
+        }
+    }
+}
