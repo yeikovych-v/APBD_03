0228c19fd77baf7869ccbb44ad6f27c78ab3b8e1
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:08:14 +0100

    Added Repository to store containers.

---
+++ APBD_03/repository/ContainerRepository.cs
@@ -0,0 +1,33 @@
+﻿using APBD_03.model.containers;
+
+namespace APBD_03.model.repository;
+
+public static class ContainerRepository
+{
+    private static List<Container> _containers = new();
+    
+    public static void Add(Container container)
+    {
+        _containers.Add(container);
+    }
+    
+    public static void AddAll(List<Container> containers)
+    {
+        foreach (var c in containers) Add(c);
+    }
+
+    public static Container? FindBySerialNum(ContainerSerialNumber serialNumber)
+    {
+        foreach (var c in _containers)
+        {
+            if (c.SerialNum == serialNumber) return c;
+        }
+
+        return null;
+    }
+
+    public static List<Container> FindAll()
+    {
+        return _containers;
+    }
+}
