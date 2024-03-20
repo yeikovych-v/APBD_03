3292961847b42bf3ef9bf1428e1bb2573ecf526a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:08:40 +0100

    Added Repository to store ships and do CRUD operations on stored data.

---
+++ APBD_03/repository/ShipRepository.cs
@@ -0,0 +1,23 @@
+﻿using APBD_03.model;
+
+namespace APBD_03.repository;
+
+public static class ShipRepository
+{
+    private static List<Ship> _ships = new();
+
+    public static void Add(Ship ship)
+    {
+        _ships.Add(ship);
+    }
+
+    public static void AddAll(List<Ship> ships)
+    {
+        _ships.AddRange(ships);
+    }
+    
+    public static List<Ship> FindAll()
+    {
+        return _ships;
+    }
+}
