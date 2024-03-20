1c3755489ade86bb8de192e28161d4bda817bbae
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:09:27 +0100

    Added Container implementation - Liquid Hazardous Container with needed methods.

---
+++ APBD_03/model/containers/LiquidHazardousContainer.cs
@@ -0,0 +1,30 @@
+﻿using APBD_03.exception;
+using APBD_03.service;
+
+namespace APBD_03.model.containers;
+
+public class LiquidHazardousContainer(
+    decimal heightCm,
+    decimal weightKg,
+    decimal depthCm,
+    decimal maxPayloadKg)
+    : LiquidContainer(heightCm, weightKg, depthCm, maxPayloadKg),
+        IHazardNotifier
+{
+    public void SendTextNotification()
+    {
+        Console.WriteLine("A Hazardous Situation has occured with LiquidHazardousContainer: [" + SerialNum + "]");
+    }
+    
+    public override void LoadCargo(decimal payload)
+    {
+        var newPayload = CargoMassKg + payload;
+        if (newPayload > (MaxPayloadKg * 0.5m))
+        {
+            ReportService.ReportDangerousSituation("LiquidHazardousContainer was tried to be loaded more than 50%.");
+            return;
+        }
+        CargoMassKg = newPayload;
+        Console.WriteLine($"Cargo loaded on ==> {SerialNum}");
+    }
+}
