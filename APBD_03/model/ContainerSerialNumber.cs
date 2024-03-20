e7ced4bc560e50002750fae617447ff0dfb90e13
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:11:41 +0100

    Added class for storing Container Serial Number and some methods to retrieve data from it.

---
+++ APBD_03/model/ContainerSerialNumber.cs
@@ -0,0 +1,45 @@
+﻿using APBD_03.service;
+
+namespace APBD_03.model;
+
+public class ContainerSerialNumber(ContainerType type)
+{
+    private string FirstPart { get; } = "KON";
+    private ContainerType Type { get; } = type;
+    private int Id { get; } = SerialNumGeneratorService.GenerateSerialNumFor(type);
+
+    public override string ToString()
+    {
+        return FirstPart + "-" + CharFromType(Type) + "-" + Id;
+    }
+
+    private string CharFromType(ContainerType containerType)
+    {
+        return containerType switch
+        {
+            ContainerType.Refrigerated => "R",
+            ContainerType.Gas => "G",
+            ContainerType.Liquid => "L",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        ContainerSerialNumber o = (ContainerSerialNumber)obj;
+
+        return FirstPart == o.FirstPart &&
+               Type == o.Type &&
+               Id == o.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstPart, Type, Id);
+    }
+}
