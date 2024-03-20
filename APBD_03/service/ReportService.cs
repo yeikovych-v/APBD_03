288d8c3e9a60896fd6367eb7df87762ea6bf6846
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-20 14:05:38 +0100

    Added service to send reports.

---
+++ APBD_03/service/ReportService.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_03.service;
+
+public static class ReportService
+{
+    public static void ReportDangerousSituation(string message) {
+        Console.Write("REPORT :: ");
+        Console.WriteLine(message);
+    }
+}
