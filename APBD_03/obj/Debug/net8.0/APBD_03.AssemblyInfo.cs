acfa6bfc225caab4c9d7196c564efdd5caf8bb7a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-21 14:55:07 +0100

    Updated configs.

--- APBD_03/obj/Debug/net8.0/APBD_03.AssemblyInfo.cs
+++ APBD_03/obj/Debug/net8.0/APBD_03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD_03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fe8248b303064b5c780f4810f582a863c184ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD_03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD_03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
