815b9d2203a2116484b3de22cf7a4b07483469d8
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-21 14:59:34 +0100

    Moved most methods to console controller.

--- APBD_03/Program.cs
+++ APBD_03/Program.cs
@@ -1,4 +1,5 @@
-﻿using APBD_03.model;
+﻿using APBD_03.controller;
+using APBD_03.model;
 using APBD_03.model.containers;
 using APBD_03.model.repository;
 using APBD_03.repository;
@@ -9,92 +10,8 @@ public class Program
     public static void Main(string[] args)
     {
         InitDb();
-        PrintSeparator();
-        
-        LoadCargo();
-        PrintSeparator();
-        
-        LoadCargoOnShips();
-        PrintSeparator();
-        
-        RemoveCargoFromShips();
-        PrintSeparator();
-
-        UnloadCargo();
-        PrintSeparator();
-
-        LoadCargoToShipAndReplaceToOther();
-        PrintSeparator();
-    }
-
-    private static void LoadCargoToShipAndReplaceToOther()
-    {
-        var shipOne = ShipRepository.FindAll().First();
-        var shipTwo = ShipRepository.FindAll().Last();
-        var container = ContainerRepository.FindAll().First();
-
-        shipOne.Add(container);
-        Console.WriteLine("Before transfer: ");
-        Console.WriteLine("");
-        Console.WriteLine("SHIP 01: ");
-        Console.WriteLine(shipOne);
-        Console.WriteLine("");
-        Console.WriteLine("SHIP 02: ");
-        Console.WriteLine(shipTwo);
-        ShipService.TransferContainerFromTo(shipOne, shipTwo, container);
-        Console.WriteLine("After transfer: ");
-        Console.WriteLine("");
-        Console.WriteLine("SHIP 01: ");
-        Console.WriteLine(shipOne);
-        Console.WriteLine("");
-        Console.WriteLine("SHIP 02: ");
-        Console.WriteLine(shipTwo);
-    }
-
-    private static void UnloadCargo()
-    {
-        foreach (var c in ContainerRepository.FindAll())
-        {
-            c.UnloadCargo();
-        }
-    }
-
-    private static void PrintSeparator()
-    {
-        Console.WriteLine("============================================================================================================================================");
-    }
-
-    private static void RemoveCargoFromShips()
-    {
-        foreach (var s in ShipRepository.FindAll())
-        {
-            foreach (var c in ContainerRepository.FindAll())
-            {
-                s.Remove(c);
-            }
-        }
-    }
-
-    private static void LoadCargoOnShips()
-    {
-        foreach (var s in ShipRepository.FindAll())
-        {
-            s.AddAll(ContainerRepository.FindAll());
-        }
-    }
-
-    private static void LoadCargo()
-    {
-        foreach (var c in ContainerRepository.FindAll())
-        {
-            if (c is RefrigeratorContainer)
-            {
-                RefrigeratorContainer refrigerator = (RefrigeratorContainer)c;
-                refrigerator.LoadCargo(20, Product.Bananas);
-                continue;
-            }
-            c.LoadCargo(20);
-        }
+        ConsoleCommandsController.PrintSeparator();
+        ConsoleCommandsController.InitConsoleInterface();
     }
 
     private static void InitDb()
